f55541fc30d2c6c1c737a4f7f1f557368e8e0eea
Kari Igiński <charlinski@outlook.com>
2023-05-19 01:01:54 +0200

    Add project files.

---
+++ ChalengeAPP/Program.cs
@@ -0,0 +1,22 @@
+﻿// Kobieta poniżej 30 lat 
+// Ewa lat 33
+// Niepełnoletni mężczyzna 
+
+
+using System.ComponentModel.Design;
+
+string name = "Ewa";
+int age = 33;
+string gender = "Kobieta";
+
+if (age < 30) ;
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (name == "Ewa" && age > 33) ;
+{
+    Console.WriteLine("Ewa lat 33");
+}
+if (!(gender == "Kobieta")) ;
+else
+    Console.WriteLine("niepełnoletni mężczyzna");
