008cc1ceb4ed532e24963635e58e6bf6783de59f
GokumasterBlackNinja <132934846+GokumasterBlackNinja@users.noreply.github.com>
2023-05-23 18:24:39 +0200

    Update Program.cs

--- ChalengeAPP/Program.cs
+++ ChalengeAPP/Program.cs
@@ -2,19 +2,23 @@
 // Ewa lat 33
 // Niepełnoletni mężczyzna 
 
+using System;
 
 string name = "Ewa";
-int age1 = 29;
-int age2 = 33;
+int age = 33;
 string gender = "Kobieta";
 
-
-if (age1 < 30)
+if (age < 30 && gender == "Kobieta")
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
- if (name == "Ewa" && age2 == 33)
+
+if (name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-    Console.WriteLine("niepełnoletni mężczyzna");
+else if (gender == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
